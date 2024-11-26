b8b099991255240f1b5c25862d7085fa59181049
Griff-123 <griffinstechmesser@gmail.com>
2024-11-25 23:03:08 -0600

    Created a display and added a gitignore

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import pygame
-import constants
+from constants import *
+x = True
 
 def main():
-    print(f"Starting asteroids!\nScreen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}")
+    x = True
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    while x == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+
+
+
 
 if __name__ == "__main__":
     main()
