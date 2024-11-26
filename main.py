0dc5316e716bb20bb17211ced61ed2747a6bce77
Griff-123 <griffinstechmesser@gmail.com>
2024-11-25 23:48:39 -0600

    Added fps

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ x = True
 def main():
     x = True
     pygame.init
+    fps = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     while x == True:
@@ -14,6 +16,8 @@ def main():
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         screen.fill((0,0,0))
         pygame.display.flip()
+        fps.tick(60)
+        dt = fps.get_time() / 1000
 
 
 
