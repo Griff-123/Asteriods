449268b43ca7dcd3483bc7d1c3dcb3eeb0e0e4d1
Griff-123 <griffinstechmesser@gmail.com>
2024-11-28 22:59:27 -0600

    added containers and groups

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 def main():
     x = True
     pygame.init
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    Player.containers = (updatable_group, drawable_group)
     user = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     fps = pygame.time.Clock()
     dt = 0
@@ -16,9 +19,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        user.draw(screen)
+        for user in drawable_group:
+            user.draw(screen)
         pygame.display.flip()
-        user.update(dt)
+        for user in updatable_group:
+            user.update(dt)
         fps.tick(60)
         dt = fps.get_time() / 1000
 
